288b70b9b349236078018c29a392632869e77b2e
RailonA <railonacosta@gmail.com>
2022-01-31 16:46:11 -0500

    React working

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NavBar = () => (
+  <div className="headerNavbar  p-2  text-white">
+    <h1 className="m-2 text-center headerText">Hero Selecter</h1>
+  </div>
+);
+
+export default NavBar;
---
+++ app/javascript/pages/homePage.js
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+import React from 'react'
+
+const HomePage = () => {
+
+  return (
+    <div>
+      <div>
+        <h1>REACT</h1>
+        <h1>TESTINGGGG</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
